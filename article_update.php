8f700d3d34dbd29a9b7eea274704ca13a8596442
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-07-04 14:42:51 +0200

    vérification de l'existence de l'id

--- article_update.php
+++ article_update.php
@@ -5,17 +5,23 @@ require_once './model/article.php';
 require_once './model/auteur.php';
 require_once './model/categorie.php';
 
-// récupération de l'id qui se trouve dans l'url
-$id = htmlspecialchars($_GET['id']);
+// récupération de l'id dans l'url si existant et redirection dans le cas contraire
+if(isset($_GET['id'])){
+    $id = htmlspecialchars($_GET['id']);
+}else{
+    header('Location:index.php');
+    exit();
+}
 
 $articleModel = new Article();
 // récupération des informations de l'article en fonction de l'ID
 $article =  $articleModel->getArticle($id);
 
-// récupération des auteurs en BDD
+// récupération de tous les auteurs
 $authorModel = new Auteur();
 $authorList = $authorModel->getAllAuthor();
 
+// récupération de toutes les catégories
 $categoryModel = new Category();
 $categoryList = $categoryModel->getAllCategory();
 
